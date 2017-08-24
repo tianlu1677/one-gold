4190b1e04f62370fd6f20c9eb4ef6c2c27ec81f3
tianlu1677 <tianlu1677@gmail.com>
2017-08-24 17:58:35 +0800

    wexin init

---
+++ App.js
@@ -0,0 +1,14 @@
+App({
+  onLaunch: function() {
+    // Do something initial when launch.
+   
+    
+  },
+  onShow: function() {
+      // Do something when show.
+  },
+  onHide: function() {
+      // Do something when hide.
+  },
+  globalData: 'I am global data'
+})
